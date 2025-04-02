64d39fd0c8379e91d98d4168c9e8b1a76124a27c
MaramRezgui1 <maram.rezgui@esprit.tn>
2025-04-02 22:05:12 +0200

    frontOffice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Importation des composants
+import { AllTemplateFrontComponent } from './FrontOffice/all-template-front/all-template-front.component';
+import { AllTemplateBackComponent } from './BackOffice/all-template-back/all-template-back.component';
+import { HomeFrontComponent } from './FrontOffice/home-front/home-front.component';
+
+const routes: Routes = [
+  { path: '', component: AllTemplateFrontComponent ,
+    children: [
+      { path: '', component: HomeFrontComponent }
+    ]
+  },
+  { path: 'admin', component: AllTemplateBackComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderFrontComponent } from './FrontOffice/header-front/header-front.component';
+import { FooterFrontComponent } from './FrontOffice/footer-front/footer-front.component';
+import { AllTemplateFrontComponent } from './FrontOffice/all-template-front/all-template-front.component';
+import { NavbarBackComponent } from './BackOffice/navbar-back/navbar-back.component';
+import { SidebarBackComponent } from './BackOffice/sidebar-back/sidebar-back.component';
+import { FooterBackComponent } from './BackOffice/footer-back/footer-back.component';
+import { AllTemplateBackComponent } from './BackOffice/all-template-back/all-template-back.component';
+import { HomeFrontComponent } from './FrontOffice/home-front/home-front.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderFrontComponent,
+    FooterFrontComponent,
+    AllTemplateFrontComponent,
+    NavbarBackComponent,
+    SidebarBackComponent,
+    FooterBackComponent,
+    AllTemplateBackComponent,
+    HomeFrontComponent
   ],
   imports: [
     BrowserModule,
