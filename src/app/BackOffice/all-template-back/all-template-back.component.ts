609db3a91d38d69b64887ff9140d72ac41a797f8
Nourelhouda <nour.farhat@esprit.tn>
2025-04-05 20:06:10 +0100

    Delete unused template files

--- src/app/BackOffice/all-template-back/all-template-back.component.ts
+++ src/app/BackOffice/all-template-back/all-template-back.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./all-template-back.component.css']
 })
 export class AllTemplateBackComponent {
-
+  loadScript(path: string) {
+    const script = document.createElement('script');
+    script.src = path;
+    script.async = true;
+    document.body.appendChild(script);
+  }
+  
+  ngAfterViewInit(): void {
+    this.loadScript('assets/BackOffice/purple-free/src/assets/js/dashboard.js');
+    this.loadScript('assets/BackOffice/purple-free/src/assets/js/off-canvas.js');
+    this.loadScript('assets/BackOffice/purple-free/src/assets/js/todolist.js');
+  }
+  
+  
 }
