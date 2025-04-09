5e715a092bcc90c47e23b773c6719e75e2151c6f
Nourelhouda <nour.farhat@esprit.tn>
2025-04-09 16:11:11 +0100

    cruds+tri+search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,13 @@ import { AppComponent } from './app.component';
 import { FrontOfficeModule } from './front-office/front-office.module';
 import { BackOfficeModule } from './back-office/back-office.module';
 import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ConfirmationComponent,
-
+AppComponent,
+ConfirmationComponent
   ],
   imports: [
     BrowserModule, // Ensure this is only imported in the root module
@@ -20,7 +20,7 @@ import { ConfirmationComponent } from './confirmation/confirmation.component';
     AppRoutingModule,
     FrontOfficeModule,
     BackOfficeModule,
-
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
