877db0f5eaf0866fe4015fe33820e71ad13aa3db
NidhalNaffati <nidhalnaffati@gmail.com>
2025-04-14 16:25:14 +0100

    feat: front-office & back-office ready to be used.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,36 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 // Importation des composants
-import { AllTemplateFrontComponent } from './FrontOffice/all-template-front/all-template-front.component';
-import { AllTemplateBackComponent } from './BackOffice/all-template-back/all-template-back.component';
-import { HomeFrontComponent } from './FrontOffice/home-front/home-front.component';
+import {AllTemplateFrontComponent} from './FrontOffice/all-template-front/all-template-front.component';
+import {AllTemplateBackComponent} from './BackOffice/all-template-back/all-template-back.component';
+import {HomeFrontComponent} from './FrontOffice/home-front/home-front.component';
+import {HomeBackComponent} from './BackOffice/home-back/home-back.component';
 
 const routes: Routes = [
-  { path: '', component: AllTemplateFrontComponent ,
+  {
+    path: '', component: AllTemplateFrontComponent,
     children: [
-      { path: '', component: HomeFrontComponent }
+      {path: '', component: HomeFrontComponent},
+      // {path:'add-appointment', component: AddAppointmentComponent},
     ]
   },
-  { path: 'admin', component: AllTemplateBackComponent }
+
+  // { path: 'calendar', component: CalendarComponent },
+
+  {
+    path: 'admin', component: AllTemplateBackComponent,
+    children: [
+      {path: '', component: HomeBackComponent},
+      //  { path: 'appointments', component: AppointmentListComponent },
+      //  { path: 'appointments/edit/:id', component: EditAppointmentComponent },
+    ]
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderFrontComponent } from './FrontOffice/header-front/header-front.component';
-import { FooterFrontComponent } from './FrontOffice/footer-front/footer-front.component';
-import { AllTemplateFrontComponent } from './FrontOffice/all-template-front/all-template-front.component';
-import { NavbarBackComponent } from './BackOffice/navbar-back/navbar-back.component';
-import { SidebarBackComponent } from './BackOffice/sidebar-back/sidebar-back.component';
-import { FooterBackComponent } from './BackOffice/footer-back/footer-back.component';
-import { AllTemplateBackComponent } from './BackOffice/all-template-back/all-template-back.component';
-import { HomeFrontComponent } from './FrontOffice/home-front/home-front.component';
-import { HeaderBackComponent } from './BackOffice/header-back/header-back.component';
-import { HomeBackComponent } from './BackOffice/home-back/home-back.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderFrontComponent} from './FrontOffice/header-front/header-front.component';
+import {FooterFrontComponent} from './FrontOffice/footer-front/footer-front.component';
+import {AllTemplateFrontComponent} from './FrontOffice/all-template-front/all-template-front.component';
+import {NavbarBackComponent} from './BackOffice/navbar-back/navbar-back.component';
+import {SidebarBackComponent} from './BackOffice/sidebar-back/sidebar-back.component';
+import {FooterBackComponent} from './BackOffice/footer-back/footer-back.component';
+import {AllTemplateBackComponent} from './BackOffice/all-template-back/all-template-back.component';
+import {HomeFrontComponent} from './FrontOffice/home-front/home-front.component';
+import {HomeBackComponent} from './BackOffice/home-back/home-back.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FullCalendarModule} from '@fullcalendar/angular';
 
 @NgModule({
   declarations: [
@@ -25,14 +27,18 @@ import { HomeBackComponent } from './BackOffice/home-back/home-back.component';
     FooterBackComponent,
     AllTemplateBackComponent,
     HomeFrontComponent,
-    HeaderBackComponent,
-    HomeBackComponent
+    HomeBackComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FullCalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
