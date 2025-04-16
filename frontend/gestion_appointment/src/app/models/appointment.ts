344bb9bb83055dae8442df7c10492125feabe6e4
NidhalNaffati <nidhalnaffati@gmail.com>
2025-04-16 02:36:30 +0100

    deleted the wrong frontend directory

--- frontend/gestion_appointment/src/app/models/appointment.ts
+++
@@ -1,9 +0,0 @@
-export class Appointment {
-    id?: number;
-    date!: string;       // LocalDate -> string (format YYYY-MM-DD)
-    startTime!: string;  // LocalTime -> string (format HH:mm:ss)
-    endTime!: string;    // LocalTime -> string (format HH:mm:ss)
-    userId!: number;
-    isAvailable!: boolean;
-  }
-  
