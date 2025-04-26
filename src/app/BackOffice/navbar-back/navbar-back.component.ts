50283b7b6090fd58a24f7617629fc29cd028c6c5
NidhalNaffati <nidhalnaffati@gmail.com>
2025-04-26 11:44:44 +0100

    feat: show u the admin reel name instead of the static name.

--- src/app/BackOffice/navbar-back/navbar-back.component.ts
+++ src/app/BackOffice/navbar-back/navbar-back.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../auth/auth.service";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-navbar-back',
   templateUrl: './navbar-back.component.html',
   styleUrls: ['./navbar-back.component.css']
 })
-export class NavbarBackComponent {
+export class NavbarBackComponent implements OnInit {
+  adminFullName: string = 'Admin User';
+  apiUrl = environment.apiUrl;
+
+  constructor(
+    private authService: AuthService,
+    private http: HttpClient
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.loadAdminFullName();
+  }
+
+  loadAdminFullName(): void {
+    const userId = this.authService.getUserId() || 1;
+
+    const params = new HttpParams().set('id', userId.toString());
+
+    this.http.get(`${this.apiUrl}/api/v1/admin/fullname`, {
+      params,
+      responseType: 'text'
+    })
+      .subscribe({
+        next: (response: string) => {
+          this.adminFullName = response;
+        },
+        error: (error) => {
+          console.error('Error fetching admin name:', error);
+        }
+      });
+
+  }
+
 
 }
--- src/app/BackOffice/sidebar-back/sidebar-back.component.ts
+++ src/app/BackOffice/sidebar-back/sidebar-back.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {environment} from "../../../environments/environment";
+import {AuthService} from "../../auth/auth.service";
+import {HttpClient, HttpParams} from "@angular/common/http";
 
 @Component({
   selector: 'app-sidebar-back',
@@ -6,5 +9,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar-back.component.css']
 })
 export class SidebarBackComponent {
+  adminFullName: string = 'Admin User';
+  apiUrl = environment.apiUrl;
+
+  constructor(
+    private authService: AuthService,
+    private http: HttpClient
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.loadAdminFullName();
+  }
+
+  loadAdminFullName(): void {
+    const userId = this.authService.getUserId() || 1;
+
+    const params = new HttpParams().set('id', userId.toString());
+
+    this.http.get(`${this.apiUrl}/api/v1/admin/fullname`, {
+      params,
+      responseType: 'text'
+    })
+      .subscribe({
+        next: (response: string) => {
+          this.adminFullName = response;
+        },
+        error: (error) => {
+          console.error('Error fetching admin name:', error);
+        }
+      });
+
+  }
+
 
 }
